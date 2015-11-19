7942f14ad62f6b9f60a131a45c6a63ad166052ec
Dmytro Mitin <dmitin3@gmail.com>
2015-11-19 16:31:04 +0200

    Reinitialized git repository

---
+++ tanks/Bullet.java
@@ -0,0 +1,50 @@
+package tanks;
+
+public class Bullet {
+	
+	public final static int SPEED = 5;
+	
+	private int x;
+
+	private int y;
+	
+	private Direction direction;
+
+	public Bullet(int x, int y, Direction direction) {
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+	}
+
+	public Bullet(Tank tank) {
+		this(tank.getX() + 25 + 7 * tank.getDirection().stepX,
+			 tank.getY() + 25 + 7 * tank.getDirection().stepY,
+			 tank.getDirection());
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+
+	public Direction getDirection() {
+		return direction;
+	}
+
+	public void updateX(int step) {
+		x += step;
+	}
+
+	public void updateY(int step) {
+		y += step;
+	}
+
+	public void destroy() {
+		x = -100;
+		y = -100;
+	}
+	
+}
